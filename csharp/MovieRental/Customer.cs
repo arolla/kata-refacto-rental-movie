834faaf3cf6ce753b6397cdfc9cbfc86c313fe36
MacReiben <macreiben@gmail.com>
2022-12-12 14:17:54 +0100

    initial version after pvi started.

--- csharp/MovieRental/Customer.cs
+++ csharp/MovieRental/Customer.cs
@@ -4,70 +4,69 @@ namespace MovieRental
 {
     public class Customer
     {
-
-        private string _name;
-        private List<Rental> _rentals = new List<Rental>();
-
+        private readonly string _name;
+        private readonly List<Rental> _rentals = new List<Rental>();
         public Customer(string name)
         {
             _name = name;
         }
-
-        public void addRental(Rental arg)
+        public void AddRental(Rental arg)
         {
             _rentals.Add(arg);
         }
-
-        public string getName()
+        public string GetName()
         {
             return _name;
         }
-
-        public string statement()
+        public string PrintRentalRecord()
         {
             double totalAmount = 0;
-            int frequentRenterPoints = 0;
-            string result = "Rental Record for " + getName() + "\n";
-
-            foreach (Rental each in _rentals)
+            var frequentRenterPoints = 0;
+            var result = "Rental Record for " + GetName() + "\n";
+            foreach (var rental in _rentals)
             {
                 double thisAmount = 0;
-
-                //determine amounts for each line
-                switch (each.getMovie().getPriceCode())
-                {
-                    case Movie.REGULAR:
-                        thisAmount += 2;
-                        if (each.getDaysRented() > 2)
-                            thisAmount += (each.getDaysRented() - 2) * 1.5;
-                        break;
-                    case Movie.NEW_RELEASE:
-                        thisAmount += each.getDaysRented() * 3;
-                        break;
-                    case Movie.CHILDRENS:
-                        thisAmount += 1.5;
-                        if (each.getDaysRented() > 3)
-                            thisAmount += (each.getDaysRented() - 3) * 1.5;
-                        break;
-                }
-
+                thisAmount = CalculateMovieRentalAmount(rental, thisAmount);
                 // add frequent renter points
                 frequentRenterPoints++;
                 // add bonus for a two day new release rental
-                if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+                if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+                {
                     frequentRenterPoints++;
-
+                }
                 // show figures for this rental
-                result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString() + "\n";
+                result += "\t" + rental.getMovie().getTitle() + "\t" + thisAmount + "\n";
                 totalAmount += thisAmount;
             }
-
             // add footer lines
-            result += "Amount owed is " + totalAmount.ToString() + "\n";
-            result += "You earned " + frequentRenterPoints.ToString() + " frequent renter points";
-
+            result += "Amount owed is " + totalAmount + "\n";
+            result += "You earned " + frequentRenterPoints + " frequent renter points";
             return result;
         }
+        private static double CalculateMovieRentalAmount(Rental rental, double thisAmount)
+        {
+            //determine amounts for each line
+            switch (rental.getMovie().getPriceCode())
+            {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (rental.getDaysRented() > 2)
+                    {
+                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += rental.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (rental.getDaysRented() > 3)
+                    {
+                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+            return thisAmount;
+        }
     }
-
 }
